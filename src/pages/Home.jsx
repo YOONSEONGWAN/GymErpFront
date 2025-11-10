8ab7545045b1aa150ae73d1ae3376ea6bafa4f56
jong9 <jonghunseo@naver.com>
2025-11-10 16:43:22 +0900

    styles 폴더 삭제 이슈 커밋

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -82,8 +82,7 @@ useEffect(() => {
         {/* 이하 동일 */}
         
         <div className="row row-cols-1 row-cols-xl-2 g-4">
-
-<div className="col">
+          <div className="col">
             <div className="glass soft-shadow">
              
               <div className="card-body dashboard-sizer ai-tall">
