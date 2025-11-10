66ff16d7c16f14b115eaabf97a72d418c2eb455c
JungSu94 <wjdtn220@naver.com>
2025-11-10 17:51:18 +0900

    push 전 커밋

--- src/pages/PostEdit.jsx
+++ src/pages/PostEdit.jsx
@@ -227,7 +227,6 @@ export default function PostEdit() {
                     style={textarea}
                   />
                 </div>
-
                 {/* 미리보기 */}
                 {showPreview && (
                   <div>
